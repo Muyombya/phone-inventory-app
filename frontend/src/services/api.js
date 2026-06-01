1cba7c757c1f0d133db8d1448e477c2f66580e85
Muyombya Martin <muyombyamartin@gmail.com>
2026-06-01 19:23:51 +0300

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://phone-inventory-app.onrender.com/api",
 });
 
 api.interceptors.request.use(
